0890d79bb9871c532d3bc953e9f4c78824d6c8e3
VolkovVV <Volkovwladislav@mail.ru>
2020-07-07 08:35:13 +0400

    6.0.2. Заполнение массива. [#317959]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Rinat";
+        names[1] = "Yura";
+        names[2] = "Vlad";
+        names[3] = "Yulia";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
